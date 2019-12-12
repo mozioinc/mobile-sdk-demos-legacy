2d52b5cacf2ec1bb0ecffcd95d8160d80c82198d
Caio Fukelmann Landau <caio@mozio.com>
2019-12-13 04:11:21 +0700

    First try: deploy to GHP

---
+++ precache-manifest.fe121f7b1577e0508651ed4a0f828c37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e572921906b489c7edcab41be8cbeaa6",
+    "url": "/sdk_demos/index.html"
+  },
+  {
+    "revision": "5aebcebd569783bf7f28",
+    "url": "/sdk_demos/static/css/main.a346cdf0.chunk.css"
+  },
+  {
+    "revision": "66d0b7e70e27a21fd72d",
+    "url": "/sdk_demos/static/js/2.63c60244.chunk.js"
+  },
+  {
+    "revision": "e3b119147601c19e4dc97873a4612502",
+    "url": "/sdk_demos/static/js/2.63c60244.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5aebcebd569783bf7f28",
+    "url": "/sdk_demos/static/js/main.26901514.chunk.js"
+  },
+  {
+    "revision": "d70ae8a2ac12c5c649e6",
+    "url": "/sdk_demos/static/js/runtime-main.7d43bb67.js"
+  }
+]);
